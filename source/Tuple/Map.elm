1ffbb1b80f76c2a90211f26e9b8dce149cf196c2
Alan Rempel <alan.rempel@gmail.com>
2015-09-02 19:44:31 -0500

    Added documentation.

--- source/Tuple/Map.elm
+++ source/Tuple/Map.elm
@@ -1,16 +1,33 @@
 module Tuple.Map where
+{-| Just a few convenient functions for mapping over pairs.
 
+@docs mapLeft, mapRight, mapEach, mapBoth, map
+-}
+
+{-| Map over the tuple with two functions, one for each
+element.
+-}
 mapEach : (a -> a') -> (b -> b') -> (a, b) -> (a', b')
 mapEach f g (a, b) = (f a, g b)
 
+{-| Apply the given function to both elements of the tuple.
+-}
 mapBoth : (a -> a') -> (a, a) -> (a', a')
 mapBoth f = mapEach f f
 
+{-| Synonym for `mapBoth`.
+-}
 map : (a -> a') -> (a, a) -> (a', a')
 map = mapBoth
 
+{-| Apply the given function to the left element, leaving the
+right unchanged.
+-}
 mapLeft : (a -> a') -> (a, b) -> (a', b)
 mapLeft f = mapEach f identity
 
+{-| Apply the given function to the right element, leaving the
+left unchanged.
+-}
 mapRight : (b -> b') -> (a, b) -> (a, b')
 mapRight g = mapEach identity g
