8709db568893ea99727011f80f30b66f0b4f438e
Tomas Zemanovic <tzemanovic@gmail.com>
2016-01-09 15:25:59 +0000

    added functions to map over two tuples

--- source/Tuple/Map.elm
+++ source/Tuple/Map.elm
@@ -1,7 +1,7 @@
 module Tuple.Map where
 {-| Just a few convenient functions for mapping over pairs.
 
-@docs mapLeft, mapRight, mapEach, mapBoth, map
+@docs mapLeft, mapRight, mapEach, mapEach2, mapBoth, mapBoth2, map, map2
 -}
 
 {-| Map over the tuple with two functions, one for each
@@ -10,15 +10,32 @@ element.
 mapEach : (a -> a') -> (b -> b') -> (a, b) -> (a', b')
 mapEach f g (a, b) = (f a, g b)
 
+{-| Map over two tuples with two functions, one for first elements 
+and the other for second elements.
+-}
+mapEach2 : (a -> a' -> a'') -> (b -> b' -> b'') -> (a, b) -> (a', b') 
+   -> (a'', b'')
+mapEach2 f g (a, b) (c, d) = (f a c, g b d)
+
 {-| Apply the given function to both elements of the tuple.
 -}
 mapBoth : (a -> a') -> (a, a) -> (a', a')
 mapBoth f = mapEach f f
 
+{-| Apply the given function to both elements of both tuples.
+-}
+mapBoth2 : (a -> a' -> a'') -> (a, a) -> (a', a') -> (a'', a'')
+mapBoth2 f = mapEach2 f f
+
 {-| Synonym for `mapBoth`.
 -}
 map : (a -> a') -> (a, a) -> (a', a')
 map = mapBoth
+
+{-| Synonym for `mapBoth2`.
+-}
+map2 : (a -> a' -> a'') -> (a, a) -> (a', a') -> (a'', a'')
+map2 = mapBoth2
 
 {-| Apply the given function to the left element, leaving the
 right unchanged.
